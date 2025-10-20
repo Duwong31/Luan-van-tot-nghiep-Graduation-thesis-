2506a30173acba16e1a92afa9060ca41fe8e44a6
Lý Dương <lytrieuduong0310@gmail.com>
2025-10-20 16:45:23 +0700

    update storage

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 
 import 'package:Celes/main.dart';
+import 'package:Celes/utils/hive_keys.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
@@ -11,7 +12,15 @@ void initApp() async {
   MobileAds.instance.initialize();
 
   await Hive.initFlutter();
-
+  await Hive.openBox(HiveKeys.userDetailsBox);
+  await Hive.openBox(HiveKeys.translationsBox);
+  await Hive.openBox(HiveKeys.authBox);
+  await Hive.openBox(HiveKeys.languageBox);
+  await Hive.openBox(HiveKeys.themeBox);
+  await Hive.openBox(HiveKeys.svgBox);
+  await Hive.openBox(HiveKeys.jwtToken);
+  await Hive.openBox(HiveKeys.historyBox);
+  
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
     (_) async {
       SystemChrome.setSystemUIOverlayStyle(
---
+++ lib/utils/hive_keys.dart
@@ -0,0 +1,23 @@
+class HiveKeys {
+  //Box
+  static const String userDetailsBox = "userDetailsBox";
+  static const String translationsBox = "translations";
+  static const String jwtToken = "token";
+  static const String svgBox = "svgBox";
+  static const String historyBox = "historyBox";
+
+  //Auth Box
+  static const String authBox = "authBox";
+  static const String isAuthenticated = "isAuthenticated";
+  static const String isUserFirstTime = "isUserFirstTime";
+  static const String isUserSkip = "isUserSkip";
+  static const String isProfileCompleted = "isProfileCompleted";
+
+  //Language Box
+  static String languageBox = "language";
+  static String currentLanguageKey = "currentLanguage";
+
+  //Theme Box
+  static String themeBox = "themeBox";
+  static String currentTheme = "currentTheme";
+}
