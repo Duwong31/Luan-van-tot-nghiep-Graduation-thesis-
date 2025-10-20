8305232fe820701a16fcc25d8f4868c48563c286
Lý Dương <lytrieuduong0310@gmail.com>
2025-10-20 16:51:55 +0700

    update hive

---
+++ lib/utils/hive_utils.dart
@@ -0,0 +1,113 @@
+import 'package:Celes/app/app_theme.dart';
+import 'package:Celes/app/routes.dart';
+import 'package:Celes/utils/helper_utils.dart';
+import 'package:Celes/utils/hive_keys.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+
+class HiveUtils {
+  ///private constructor
+  HiveUtils._();
+
+  static String getJWT() {
+    return Hive.box(HiveKeys.userDetailsBox).get(HiveKeys.jwtToken);
+  }
+
+  static String? getUserId() {
+    return Hive.box(HiveKeys.userDetailsBox).get("id").toString();
+  }
+
+  static AppTheme getCurrentTheme() {
+    var current = Hive.box(HiveKeys.themeBox).get(HiveKeys.currentTheme);
+
+    return current == "dark" ? AppTheme.dark : AppTheme.light;
+  }
+
+  static String? getCountryCode() {
+    return Hive.box(HiveKeys.userDetailsBox).get("country_code");
+  }
+
+  static void setProfileNotCompleted() async {
+    await Hive.box(HiveKeys.userDetailsBox)
+        .put(HiveKeys.isProfileCompleted, false);
+  }
+
+  static dynamic setCurrentTheme(AppTheme theme) {
+    String newTheme = theme == AppTheme.light ? "light" : "dark";
+
+    Hive.box(HiveKeys.themeBox).put(HiveKeys.currentTheme, newTheme);
+  }
+
+  static void setUserData(Map data) async {
+    await Hive.box(HiveKeys.userDetailsBox).putAll(data);
+  }
+
+
+  static void setJWT(String token) async {
+    await Hive.box(HiveKeys.userDetailsBox).put(HiveKeys.jwtToken, token);
+  }
+
+  static void setUserIsAuthenticated(bool value) {
+    Hive.box(HiveKeys.authBox).put(HiveKeys.isAuthenticated, value);
+  }
+
+  static Future<void> setUserIsNotNew() {
+    return Hive.box(HiveKeys.authBox).put(HiveKeys.isUserFirstTime, false);
+  }
+
+  static Future<void> setUserSkip() {
+    return Hive.box(HiveKeys.authBox).put(HiveKeys.isUserSkip, true);
+  }
+
+  static Future<bool> storeLanguage(
+    dynamic data,
+  ) async {
+    Hive.box(HiveKeys.languageBox).put(HiveKeys.currentLanguageKey, data);
+    return true;
+  }
+
+  static dynamic getLanguage() {
+    return Hive.box(HiveKeys.languageBox).get(HiveKeys.currentLanguageKey);
+  }
+
+  @visibleForTesting
+  static Future<void> setUserIsNew() {
+    Hive.box(HiveKeys.authBox).put(HiveKeys.isAuthenticated, false);
+    return Hive.box(HiveKeys.authBox).put(HiveKeys.isUserFirstTime, true);
+  }
+
+  static bool isUserAuthenticated() {
+    return Hive.box(HiveKeys.authBox).get(HiveKeys.isAuthenticated) ?? false;
+  }
+
+  static bool isUserFirstTime() {
+    return Hive.box(HiveKeys.authBox).get(HiveKeys.isUserFirstTime) ?? true;
+  }
+
+  static bool isUserSkip() {
+    return Hive.box(HiveKeys.authBox).get(HiveKeys.isUserSkip) ?? false;
+  }
+
+  static void logoutUser(context,
+      {required VoidCallback onLogout, bool? isRedirect}) async {
+    await Hive.box(HiveKeys.userDetailsBox).clear();
+    HiveUtils.setUserIsAuthenticated(false);
+
+    onLogout.call();
+
+    Future.delayed(
+      Duration.zero,
+      () {
+        if (isRedirect ?? true) {
+          HelperUtils.killPreviousPages(context, Routes.login, {});
+        }
+      },
+    );
+  }
+
+  static void clear() async {
+    await Hive.box(HiveKeys.userDetailsBox).clear();
+    await Hive.box(HiveKeys.historyBox).clear();
+    HiveUtils.setUserIsAuthenticated(false);
+  }
+}
